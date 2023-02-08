58970d6d5047cff9f3640134af3a071c577bf013
Guilherme Vitória <guivitoria2010@hotmail.com>
2023-02-08 00:10:10 -0300

    feat: responsivity for component pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -16,18 +16,18 @@ export default function Pagination({
 	return (
 		<div className="flex justify-end mt-4">
 			<div
-				className="bg-previous flex items-center justify-center rounded-md hover:cursor-pointer hover:brightness-90 duration-300 h-8 w-auto mr-4 text-white pl-2 pr-4"
+				className="bg-previous flex items-center justify-center rounded-md hover:cursor-pointer hover:brightness-90 duration-300 h-10 w-auto mr-4 text-white md:text-base sm:text-sm text-xs pl-2 pr-4"
 				onClick={backFunction}
 			>
-				<CaretLeft size={28} color="#FFF" weight="bold" />
+				<CaretLeft size={20} color="#FFF" weight="bold" />
 				{backTitle}
 			</div>
 			<div
-				className="bg-next flex items-center justify-center rounded-md hover:cursor-pointer hover:brightness-90 duration-300 h-8 w-auto text-white pl-4 pr-2"
+				className="bg-next flex items-center justify-center rounded-md hover:cursor-pointer hover:brightness-90 duration-300 h-10 w-auto text-white md:text-base sm:text-sm text-xs pl-4 pr-2"
 				onClick={nextFunction}
 			>
 				{nextTitle}
-				<CaretRight size={28} color="#FFF" weight="bold" />
+				<CaretRight size={20} color="#FFF" weight="bold" />
 			</div>
 		</div>
 	)
